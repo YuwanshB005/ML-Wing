1dc92007e579d469e651aad084c4af159aa90579
YuwanshB005 <bansalyuwansh@gmail.com>
2024-08-25 16:25:48 +0530

    Fixed Bugs

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from googleapiclient.discovery import build
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import tensorflow
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 import pickle
